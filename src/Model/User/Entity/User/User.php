6aeb695f8da235354d22ea04d0d8b37e3b67afa4
Valery <zonovvaleraooo@gmail.com>
2022-11-25 15:10:09 +0700

    Make get in User

--- src/Model/User/Entity/User/User.php
+++ src/Model/User/Entity/User/User.php
@@ -4,4 +4,33 @@ namespace App\Model\User\Entity\User;
 
 class User
 {
+    /**
+     * email
+     *
+     * @var string
+     */
+    private $email;
+
+    /**
+     * passwordHash
+     *
+     * @var string
+     */
+    private $passwordHash;
+
+    public function __construct(string $email, string $hash)
+    {
+        $this->email = $email;
+        $this->passwordHash = $hash;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPasswordHash(): string
+    {
+        return $this->passwordHash;
+    }
 }
